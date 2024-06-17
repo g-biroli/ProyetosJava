13595d73014f4a5c6d35c74d255957d6b86d6dc9
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-06-17 17:22:47 +0200

    test

---
+++ Recuperacion/parte1test.java
@@ -0,0 +1,37 @@
+package extraordinaria;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+
+import extraordinaria.Parte1.ComprarAuto;
+
+	public class Parte1Test {
+	double expected;
+	int cantidad;
+	ComprarAuto compraAuto;
+		
+	
+	public Parte1Test(double expected, int cantidad, ComprarAuto compraAuto) {
+		this.expected = expected;
+		this.cantidad = cantidad;
+		this.compraAuto = new ComprarAuto("Coche", 100, 20);
+	}
+
+	public static Collection<Object[]> data(){
+		return Arrays.asList(new Object[][] {
+				{"Coche 1", 10000, 10, 5, 450.0},
+				{"Coche 2", 2000, 20, 3, 480.0}
+		});
+	}	
+	
+	@Test
+	void testCompraAuto() {						
+		assertEquals(expected, compraAuto.compraAuto(cantidad), 0.01);
+	}
+
+	
+	
+}
