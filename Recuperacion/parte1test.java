da6db74772495752b1acacfd1cb58acfb0c2133f
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-06-28 20:11:12 -0300

    1

--- Recuperacion/parte1test.java
+++ Recuperacion/parte1test.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 
 import org.junit.jupiter.api.Test;
 
-import extraordinaria.Parte1.ComprarAuto;	//test proyecto prueba
+import extraordinaria.Parte1.ComprarAuto;	
 											
 	public class Parte1Test {       //junit test
 	double expected;
