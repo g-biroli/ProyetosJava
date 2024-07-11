5817e1a819e524fff84469b8dbbb45ac45ac76e6
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-07-11 18:49:49 -0300

    Classe Mamifero

---
+++ CursoEmVideo/aula12/mamifero.java
@@ -0,0 +1,20 @@
+package aula12;
+
+public class Mamifero extends Animal {
+
+	@Override
+	public void locomover() {
+			
+	}
+
+	@Override
+	public void alimentar() {
+			
+	}
+
+	@Override
+	public void emitirSom() {
+		
+	}
+
+}
