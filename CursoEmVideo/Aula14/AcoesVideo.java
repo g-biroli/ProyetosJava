a01757d3e9b94591046e51455fb42771c56ef316
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-07-23 11:59:21 -0300

    1

---
+++ CursoEmVideo/Aula14/AcoesVideo.java
@@ -0,0 +1,11 @@
+package projetocurso;
+
+public interface AcoesVideo {
+	
+	
+	//Criando métodos abstratos na interface para herdar
+	public void play();
+	public void pause();
+	public void like();
+	
+}
